8102af0a3b210b43e58bc01c409fa2fd907d2a2e
YashwantMalve-stack <yashwantmalve81@gmail.com>
2025-10-11 18:43:13 +0530

    Use process.env.PORT for Render deployment

--- app.js
+++ app.js
@@ -98,6 +98,8 @@ app.use((err,req,res,next) => {
 
 })
 
-app.listen(8080,() => {
-    console.log("server listening to port 8080");
-})
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`server listening to port ${port}`);
+});
