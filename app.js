2cc3a67da5009b3b2e2513b526a46c0dd8c3b3ed
YashwantMalve-stack <yashwantmalve81@gmail.com>
2025-10-16 08:08:36 +0530

    Added redirect route to open Explore page directly

--- app.js
+++ app.js
@@ -83,6 +83,10 @@ app.use((req, res, next) => {
     next();
 })
 
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
+
 
 app.use("/listings", listingsRouter);
 app.use("/listings/:id/reviews", reviewsRouter);
